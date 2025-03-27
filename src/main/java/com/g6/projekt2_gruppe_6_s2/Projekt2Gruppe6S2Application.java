0f3eef1310a0a08f4b4dbd436453791402f79d7c
LucasPK <149070654+Socu22@users.noreply.github.com>
2025-03-27 10:49:19 +0100

    Initial commit

---
+++ src/main/java/com/g6/projekt2_gruppe_6_s2/Projekt2Gruppe6S2Application.java
@@ -0,0 +1,13 @@
+package com.g6.projekt2_gruppe_6_s2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt2Gruppe6S2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projekt2Gruppe6S2Application.class, args);
+	}
+
+}
