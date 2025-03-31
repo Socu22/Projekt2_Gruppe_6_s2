cd2550ccea842b8a72cbb294a81bd560c1e6521f
LucasPK <149070654+Socu22@users.noreply.github.com>
2025-03-31 17:07:58 +0200

    .

--- src/main/java/com/g6/projekt2_gruppe_6_s2/Projekt2Gruppe6S2Application.java
+++ src/main/java/com/g6/projekt2_gruppe_6_s2/Projekt2Gruppe6S2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Projekt2Gruppe6S2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Projekt2Gruppe6S2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt2Gruppe6S2Application.class, args);
+    }
 
 }
--- src/test/java/com/g6/projekt2_gruppe_6_s2/Projekt2Gruppe6S2ApplicationTests.java
+++ src/test/java/com/g6/projekt2_gruppe_6_s2/Projekt2Gruppe6S2ApplicationTests.java
@@ -1,13 +1,8 @@
 package com.g6.projekt2_gruppe_6_s2;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Projekt2Gruppe6S2ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
