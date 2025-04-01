b65a1ae278b886c54d694678c0a0459a6d95103e
LucasPK <149070654+Socu22@users.noreply.github.com>
2025-04-01 10:16:26 +0200

    DatabaseDone.done

--- src/main/resources/wishListDatabaseScript.sql
+++ src/main/resources/wishListDatabaseScript.sql
@@ -2,4 +2,4 @@ drop database if exists wishDatabase;
 create database wishDatabase;
 use wishDatabase;
 create table wishList(wish varchar(50));
-insert into wishList(wish)values ('Fing christ')
+insert into wishList(wish)values ('Fing christ');
