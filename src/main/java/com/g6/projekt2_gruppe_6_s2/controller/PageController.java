4e241a01ab54935898038041ebf35581447b1eda
LucasPK <149070654+Socu22@users.noreply.github.com>
2025-04-02 13:32:38 +0200

    User func in progress

--- src/main/java/com/g6/projekt2_gruppe_6_s2/controller/PageController.java
+++ src/main/java/com/g6/projekt2_gruppe_6_s2/controller/PageController.java
@@ -1,26 +1,29 @@
 package com.g6.projekt2_gruppe_6_s2.controller;
 
-import com.g6.projekt2_gruppe_6_s2.repository.WishRepository;
 import com.g6.projekt2_gruppe_6_s2.repository.WishRepositoryDataBase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
-
 @Controller
-public class WishController {
-    
+public class PageController {
+
+
     @Autowired
     WishRepositoryDataBase repo;
-    //WishRepository repo;
 
-    @GetMapping("/")
-    public String getIndex(Model model){
-        var wishList = new ArrayList<>();
-        wishList.addAll(repo.getWishList());
-        model.addAttribute("wishList",wishList);
-        return "index";
+
+    // Login form
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    // Login form with error
+    @GetMapping("/error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login";
     }
 }
--- src/main/java/com/g6/projekt2_gruppe_6_s2/model/User.java
+++ src/main/java/com/g6/projekt2_gruppe_6_s2/model/User.java
@@ -1,21 +1,50 @@
 package com.g6.projekt2_gruppe_6_s2.model;
 
-import java.util.ArrayList;
-
 public class User {
     int id;
     String username;
     String password;
+    String imgPath;
 
-    ArrayList<WishList> wishLists;
-
-    public User(int id, String username, String password) {
+    public User(int id, String username, String password, String imgPath) {
         this.id = id;
         this.username = username;
         this.password = password;
+        this.imgPath = imgPath;
     }
 
-    public void setWishLists(ArrayList<WishList> wishLists) {
-        this.wishLists = wishLists;
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
     }
 }
