e344a6da286fd6a4ef8ae249db2906fc9c18a601
Flamingotough <simongotfred@hotmail.com>
2025-04-08 13:20:03 +0200

    db Schema update 2

--- src/main/resources/dbDummySetup.sql
+++ src/main/resources/dbDummySetup.sql
@@ -12,7 +12,20 @@ SELECT userId FROM users WHERE username = 'Simon';
 
 INSERT INTO wishes (title, description, price) VALUES
     ('Lemonade', 'A cool refreshing drink, squeezed from lemons.', 21),
-    ('Flamingo', 'A majestic pink-feathered bird with webbed feet, very long limbs, and a crooked beak.', 450);
+    ('Flamingo', 'A majestic pink-feathered bird with webbed feet, very long limbs, and a crooked beak.', 450),
+    ('Doge Plushie','',10),
+    ('Spicy Meme Sauce','',5),
+    ('T-shirt','',15),
+    ('Doge','',10),
+    ('Doge','',10),
+    ('Doge','',10),
+    ('Doge','',10),
+    ('Doge','',10),
+    ('Doge','',10),
+    ('Doge','',10),
+    ('Doge','',10),
+    ('Doge','',10),
+    ('Doge','',10);
 
-INSERT INTO wishLists (listId, wishId, userId)
-SELECT listId, wishId, userId FROM listHolders, wishes WHERE userId = 1 OR title = 'Lemonade';
+INSERT INTO wishLists (listId, wishId)
+SELECT listId, wishId FROM listHolders, wishes WHERE userId = 1 OR title = 'Lemonade';
