a00287309cf567299486c9f6887ee147d98f0f25
Flamingotough <simongotfred@hotmail.com>
2025-04-09 16:01:01 +0200

    User Registering
    - Setting up. Still incomplete

--- src/main/java/com/g6/projekt2_gruppe_6_s2/controller/UserController.java
+++ src/main/java/com/g6/projekt2_gruppe_6_s2/controller/UserController.java
@@ -17,7 +17,7 @@ import java.sql.SQLException;
 public class UserController
 {
     @Autowired
-    UserRepositoryDatabase repoUser;
+    UserRepositoryDatabase userRepo;
 
     @GetMapping("/Login")
     public String login()
@@ -36,10 +36,46 @@ public class UserController
 
         try
         {
-            User user = repoUser.getUserWithUsernamePassword(username, password);
+            User user = userRepo.getUser(username, password);
 
             if (user != null)
             {
+                HttpSession session = request.getSession();
+
+                model.addAttribute("activeUser", user);
+                session.setAttribute("activeUser", user);
+
+                model.addAttribute("loginError", false);
+                model.addAttribute("loginMsg", "");
+
+                return "redirect:/Profile";
+            }
+        }
+        catch (SQLException|IllegalArgumentException e)
+        {
+            model.addAttribute("loginError", true);
+            model.addAttribute("loginMsg", e.getMessage());
+        }
+
+        return "login"; // Return to the login page with an error message
+    }
+
+    public String register(@RequestParam("username") String username,
+                           @RequestParam("password") String password,
+                           HttpServletRequest request,
+                           Model model)
+    {
+        model.addAttribute("registerMsg", "");
+
+        try
+        {
+            User user = userRepo.getUser(username, password);
+
+            if (user == null)
+            {
+
+                user = userRepo.getUser(username, password);
+
                 HttpSession session = request.getSession();
 
                 model.addAttribute("activeUser", user);
--- src/main/java/com/g6/projekt2_gruppe_6_s2/repository/UserRepositoryDatabase.java
+++ src/main/java/com/g6/projekt2_gruppe_6_s2/repository/UserRepositoryDatabase.java
@@ -13,39 +13,73 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 @Repository
-public class UserRepositoryDatabase {
+public class UserRepositoryDatabase
+{
     @Autowired
     DataSource dataSource;
+
     @Autowired
     private View error;
 
-    public ArrayList<User> getUsers() {
+    public ArrayList<User> getUsers()
+            throws SQLException
+    {
         ArrayList<User> userList = new ArrayList<>();
         String sql = "SELECT * FROM users";
 
         try (Connection connection = dataSource.getConnection();
              PreparedStatement statement = connection.prepareStatement(sql);
-             ResultSet resultSet = statement.executeQuery()) {
-
-            while (resultSet.next()) {
+             ResultSet resultSet = statement.executeQuery())
+        {
+            while (resultSet.next())
+            {
                 User user = new User();
-                user.setId(resultSet.getInt("id"));
+
+                user.setId      (resultSet.getInt   ("id"));
                 user.setUsername(resultSet.getString("username"));
-                user.setImgPath(resultSet.getString("imgPath"));
+                user.setImgPath (resultSet.getString("imgPath"));
+
                 userList.add(user);
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             e.printStackTrace();
+            throw new SQLException("Failed to Connect to Database.");
         }
 
         return userList;
     }
 
-    public User getUserWithUsernamePassword(String username, String password)
+    public User addUser(String username, String password)
+            throws SQLException
+    {
+        if(hasUser(username)) return null; // abort if user already exists
+
+        String sql = "INSERT INTO users (username, password) VALUE ( ? , ? )";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql))
+        {
+            statement.setString(1, username);
+            statement.setString(2, password);
+
+            statement.execute();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+            throw new SQLException("Failed to Connect to Database.");
+        }
+
+        return getUser(username, password);
+    }
+
+    public User getUser(String username, String password)
             throws SQLException, IllegalArgumentException
     {
-        User user = null;
-        String sql = "SELECT * FROM users WHERE username = ? and password=?";
+        User user;
+        String sql = "SELECT * FROM users WHERE username = ? and password = ?";
 
         try (Connection connection = dataSource.getConnection();
              PreparedStatement statement = connection.prepareStatement(sql))
@@ -79,4 +113,28 @@ public class UserRepositoryDatabase {
         return user;
     }
 
+    public boolean hasUser(String username)
+            throws SQLException
+    {
+        String sql = "SELECT username FROM users WHERE username = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql))
+        {
+            statement.setString(1, username);
+
+            try (ResultSet resultSet = statement.executeQuery())
+            {
+                if (resultSet.next()) return true;
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+            throw new SQLException("Failed to Connect to Database.");
+        }
+
+
+        return false;
+    }
 }
